2d3a2ca1252ef64d51d7aed8d10656019b4fddea
Manoel Kemtsa <votre-email-github@example.com>
2026-06-18 05:25:07 +0100

    Correction bug Kotlin react-native-track-player + ajout patch-package

---
+++ android/app/.cxx/Debug/3k14246m/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/op-engineering_op-sqlite/op-engineering_op-sqliteConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET op-engineering_op-sqlite::op-sqlite)
+add_library(op-engineering_op-sqlite::op-sqlite SHARED IMPORTED)
+set_target_properties(op-engineering_op-sqlite::op-sqlite PROPERTIES
+    IMPORTED_LOCATION "C:/Users/hp/Desktop/Spotify/node_modules/@op-engineering/op-sqlite/android/build/intermediates/cxx/Debug/5l55b334/obj/arm64-v8a/libop-sqlite.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/hp/Desktop/Spotify/node_modules/@op-engineering/op-sqlite/android/build/headers/op-sqlite"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
